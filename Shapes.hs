82c788c93a3f0aa1888559597c4cf2c6128c7c11
Alex <o0o.moro.o0o@gmail.com>
2014-02-20 03:40:14 -0600

    drawing IFS now

--- Shapes.hs
+++ Shapes.hs
@@ -1,15 +1,39 @@
 module Shapes (
 Vector(..),
 Shape(..),
+drawShape,
 isPoint,
 isLine,
 isCircle
 ) where
 
---data Vec = Vec (Number,Number)
+import qualified Graphics.Rendering.OpenGL as GL
+import GHC.Float
+
 type Vector = (Double,Double)
 data Shape = Point Vector | Line Vector Vector | Circle Vector Double
 
+makeGLfloat :: Double -> GL.GLfloat
+makeGLfloat = realToFrac
+-- draws a shape with openGL. pattern matches for different shapes
+drawShape :: Shape -> IO ()
+drawShape (Point (x,y)) = do
+	--GL.loadIdentity
+	GL.renderPrimitive GL.Points $ do
+		GL.vertex $ (GL.Vertex3 (makeGLfloat x) (makeGLfloat y) 0)
+
+drawShape (Line (x1,y1) (x2,y2)) = do
+	--GL.loadIdentity
+	GL.renderPrimitive GL.Lines $ do
+		GL.vertex $ (GL.Vertex3 (makeGLfloat x1) (makeGLfloat y1) 0)
+		GL.vertex $ (GL.Vertex3 (makeGLfloat x2) (makeGLfloat y2) 0)
+
+drawShape (Circle (h,k) r) = do
+	--GL.loadIdentity
+	GL.renderPrimitive GL.Points $ do
+		GL.vertex $ (GL.Vertex3 (makeGLfloat h) (makeGLfloat k) 0)
+
+-- is___ functions to test shapes (useful for filter)
 isPoint :: Shape -> Bool
 isPoint (Point (_,_)) = True
 isPoint _ = False
--- main.hs
+++ main.hs
@@ -1,37 +1,84 @@
-import Graphics.UI.GLUT as G
+import Control.Monad
+import Control.Exception
+import qualified Graphics.Rendering.OpenGL as GL
+import Graphics.Rendering.OpenGL (($=))
+import qualified Graphics.UI.GLFW as GLFW
 
 import InputHandlers
 import Shapes
 import Functions
 
-
-
 -- drawing handler
-display :: [Shape] -> G.DisplayCallback
-display shapes = do
-	G.clear [G.ColorBuffer]
-	G.swapBuffers
+draw :: GLFW.Window -> [Shape] -> IO()
+draw win shapes = do
+	GL.clear [GL.ColorBuffer, GL.DepthBuffer]
 
--- idle loop.
-idleLoop :: Int -> IO ()
-idleLoop a = return ()
+	GL.loadIdentity
+	mapM_ drawShape shapes
+	GL.flush
+	GLFW.swapBuffers win
 
+-- resize callback
+resize :: Int -> Int -> IO ()
+resize w h = do
+	GL.viewport $= (GL.Position 0 0, GL.Size (fromIntegral w) (fromIntegral h))
+	GL.matrixMode $= GL.Projection
+	GL.loadIdentity
+	GL.perspective 45 (fromIntegral w / fromIntegral h) 1 100
+	GL.matrixMode $= GL.Modelview 0
+
+-- some basic shapes.
+shapes1 =  		[Line (-1,0) (1,0)
+			,Line (1,0) (1,1)
+			,Point (0.5,0.5)
+			,Point (0.25,0.25)]
+koch =		[Line (0,0) (1/3,0) 
+		,Line (1/3,0) (0.5,0.5-(1/3))
+		,Line (0.5,0.5-(1/3)) (2/3,0)
+		,Line (2/3,0) (1,0)]
+
+koch2 =		[Line (0,0) (1/3,0) 
+		,Line (0.5,0.5-(1/3)) (1/3,0)
+		,Line (2/3,0) (0.5,0.5-(1/3)) 
+		,Line (2/3,0) (1,0)]
+
+koch3 =		[Line (1/3,0) (0,0)
+		,Line (0.5,0.5-(1/3)) (1/3,0)
+		,Line (2/3,0) (0.5,0.5-(1/3)) 
+		,Line (1,0) (2/3,0)]
+
+-- main program loop.
+mainLoop :: GLFW.Window -> Double -> Double -> Vector -> IO ()
+mainLoop win r s (tx,ty) = do
+	putStrLn "Loop:"
+	--draw win shapes1
+	draw win $ concat [iterateSystem baseline2 koch 2,iterateSystem baseline3 koch2 4, iterateSystem baseline koch3 3,[baseline2,baseline3]]
+	GLFW.pollEvents
+	putStrLn "Done."
+	mainLoop win (r-0.01) (s-0.01) (tx-0.01,ty-0.01)
+	where 	baseline = Line (-0.9,0.1) (0.9,0.1)
+		baseline2 = Line (0.5,-0.9) (0.5,0.9)
+		baseline3 = Line (0.5,0.9) (0.5,-0.9)
+		
+	
 
 -- Entry point.
 main :: IO ()
 main = do
-	-- Initialize GLUT
-	(progName, args) <- G.getArgsAndInitialize
-	G.initialDisplayMode $= [G.DoubleBuffered]
-	G.initialWindowSize $= G.Size 500 500
-	G.createWindow "Fracture Fractal Generator"
+	-- initialize GLFW. crashes with pattern match error on failure
+	True <- GLFW.init
+	window <- GLFW.createWindow 500 500 "Title" Nothing Nothing
+	case window of
+		(Just win) -> do
+			GLFW.makeContextCurrent window
+			setWindowCallbacks win
+			GLFW.swapInterval 1
+			putStrLn "Entering mainLoop..."
+			mainLoop win 0 2 (0,0)
+			GLFW.destroyWindow win
+		Nothing -> return ()
+	GLFW.terminate
+	where n = 0
+	
 
-	G.displayCallback $= display shapes
-	G.keyboardMouseCallback $= Just (basicKBHandler)
-	G.idleCallback $= Just (idleLoop 10)
-	-- Start program!
-
-	putStrLn "Entering GLUT main loop..."
-	G.mainLoop
-	where	shapes = []
 
