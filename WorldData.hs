ce68b3e186b68449b9d4dbb25947a3d260373d6a
Alex <o0o.moro.o0o@gmail.com>
2014-02-22 23:25:03 -0600

    Changed BG color and added params to System

--- WorldData.hs
+++ WorldData.hs
@@ -7,7 +7,8 @@ Color,
 System(..),
 World(..),
 WorldState,
-newWorld
+newWorld,
+newSystem
 ) where
 
 import Control.Concurrent.STM as STM
@@ -15,17 +16,25 @@ import Shapes
 
 -- define some data
 data FillType = Fill|Outline deriving (Eq)
-data System = System {baseShape::Shape,replaceShapes::[Shape],sysColor::Color,fillType::FillType} deriving (Eq)
+data System = System {baseShape::Shape,replaceShapes::[Shape],sysColor::Color,fillType::FillType, iter::Int, drawShapes::TVar [Shape]} deriving (Eq)
 
 data World = World {systems::[System], selectedSys::System, selectedPoint::Maybe Int, mouseDragging::Bool, mouseDown::Bool, firstPos::(Double,Double),lastPos::(Double,Double)}
 type WorldState = TVar World
 
-defaultSys = System {baseShape=Point (0,0),replaceShapes=[Point (0,0)],sysColor=(255,255,255),fillType=Outline}
+
+
+newSystem :: Shape -> [Shape] -> IO System
+newSystem shape rShapes = do
+	s <- newTVarIO []
+	return System{baseShape=shape,replaceShapes=rShapes,sysColor=(0,0,0),fillType=Outline,iter=1,drawShapes=s}
+
 newWorld :: [System] -> IO WorldState
-newWorld [] = newTVarIO World {systems=[], selectedSys=defaultSys, selectedPoint=Nothing,mouseDragging=False,mouseDown=False,firstPos=(0,0),lastPos=(0,0)}
+newWorld [] = do
+	defaultSys <- newSystem (Point (0,0)) [Point (0,0)]
+	newTVarIO World {systems=[], selectedSys=defaultSys, selectedPoint=Nothing,mouseDragging=False,mouseDown=False,firstPos=(0,0),lastPos=(0,0)}
 newWorld (s:xs) = newTVarIO World {systems=s:xs, selectedSys=s, selectedPoint=Nothing,mouseDragging=False,mouseDown=False,firstPos=(0,0),lastPos=(0,0)}
 
--- some system shapse.
+-- some default system shapes.
 koch =		[Line (0,0) (1/3,0) 
 		,Line (1/3,0) (0.5,0.5-(1/3))
 		,Line (0.5,0.5-(1/3)) (2/3,0)
--- main.hs
+++ main.hs
@@ -10,33 +10,55 @@ import Shapes
 import Functions
 import WorldData
 
-defaultSys =	[System {baseShape=bs1,replaceShapes=koch,sysColor=(0,0,0),fillType=Fill}
-		,System {baseShape=bs3,replaceShapes=koch2,sysColor=(0,0,0),fillType=Fill}
-		,System {baseShape=bs2,replaceShapes=koch3,sysColor=(0,0,0),fillType=Fill}]
+--defaultSys =	[System {baseShape=bs1,replaceShapes=koch,sysColor=(0,0,0),fillType=Fill}
+		--,System {baseShape=bs3,replaceShapes=koch2,sysColor=(0,0,0),fillType=Fill}
+		--,System {baseShape=bs2,replaceShapes=koch3,sysColor=(0,0,0),fillType=Fill}]
+defaultSys :: IO [System]
+defaultSys = do
+	sys1 <- newSystem bs1 koch
+	sys2 <- newSystem bs2 koch2
+	sys3 <- newSystem bs3 koch3
+	return [sys1,sys2,sys3]
 	where	bs1=Line (50,50) (150,150)
-		bs2=Line (0,0) (200,200)
-		bs3 = Line (200,0) (0,200)
+		bs2=Line (100,100) (200,200)
+		bs3 = Line (200,50) (50,200)
 
 -- drawing handler. draws shapes.
-draw :: GLFW.Window -> [Shape] -> Shape -> IO()
-draw win shapes selShape = do
-	GL.clear [GL.ColorBuffer, GL.DepthBuffer]
-
+drawShapes :: GLFW.Window -> [Shape] -> Shape -> IO()
+drawShapes win shapes selShape = do
 	GL.loadIdentity
 	mapM_ (drawShape (255,255,255)) shapes 
 	drawShape (255,0,0) selShape 
 	GL.flush
-	GLFW.swapBuffers win
+
+drawSys :: System -> IO ()
+drawSys sys = do
+	let	c = sysColor sys
+		s = baseShape sys
+	drawShape c s
+	return ()
+	
+drawSystems :: GLFW.Window -> [System] -> IO ()
+drawSystems win sys = do
+	GL.loadIdentity
+	mapM_ (drawSys) sys
+	GL.flush
 
 -- main program loop. Nothing interesting. InputHandlers is more interesting.
 mainLoop :: STM.TChan InputMsg -> WorldState -> GLFW.Window -> IO ()
 mainLoop ch worldState win = do
 	GLFW.waitEvents
-	(shapes,selShape, test) <- STM.atomically $ do
+	(sys, test) <- STM.atomically $ do
 		world <- STM.readTVar worldState
-		return $ (map (baseShape) (systems world), baseShape (selectedSys world), selectedPoint world)
+		return $ ((selectedSys world):(systems world), selectedPoint world)
 		
-	draw win shapes selShape
+	--draw win shapes selShape
+	
+	GL.clear [GL.ColorBuffer, GL.DepthBuffer]
+	drawSystems win sys
+	GLFW.swapBuffers win
+
+
 	putStrLn $ "Select:" ++ (show test)
 	-- close program when window closes.
 	isOpen <- return True
@@ -51,11 +73,13 @@ main = do
 	case window of
 		(Just win) -> do
 			ch <- STM.newTChanIO :: IO (STM.TChan InputMsg)
-			worldState <- newWorld defaultSys
+			sys <- defaultSys
+			worldState <- newWorld sys
 			
 			GLFW.makeContextCurrent window
 			setWindowCallbacks ch worldState win
 			GLFW.swapInterval 1
+			GL.clearColor $= GL.Color4 1 1 1 1
 			putStrLn "Entering mainLoop..."
 			mainLoop ch worldState win 
 			GLFW.destroyWindow win
