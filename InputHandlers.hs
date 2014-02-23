c357b79ca3f657ac36c70cb7dd6c7b9e75dded3e
Alex <o0o.moro.o0o@gmail.com>
2014-02-23 00:29:33 -0600

    Refactor for readability

--- InputHandlers.hs
+++ InputHandlers.hs
@@ -7,9 +7,10 @@ import qualified Graphics.Rendering.OpenGL as GL
 import Graphics.Rendering.OpenGL (($=))
 import qualified Graphics.UI.GLFW as GLFW
 import qualified Control.Concurrent.STM as STM
---import Data.StateVar hiding ($=)
+
 import WorldData
 import Shapes
+import Util
 
 -- Input messages
 data InputMsg 	=WinResize Int Int
@@ -26,6 +27,7 @@ data InputMsg 	=WinResize Int Int
 dragThresh = 5
 
 -- GLFW handlers!
+-- type declarations
 errorCallback 		:: GLFW.Error -> String -> IO ()
 windowSizeCallback	:: STM.TChan InputMsg -> WorldState -> GLFW.Window -> Int -> Int -> IO ()
 windowCloseCallback	:: STM.TChan InputMsg -> WorldState -> GLFW.Window -> IO ()
@@ -36,64 +38,66 @@ keyCallback		:: STM.TChan InputMsg -> WorldState -> GLFW.Window -> GLFW.Key -> I
 
 -- Define handlers. Where the Magic happens!
 errorCallback err str = putStrLn str
+
 -- window related callbacks
 windowSizeCallback tc _ win w h = resize w h
+
 windowCloseCallback tc _ win = GLFW.destroyWindow win
+
 windowRefreshCallback tc _ win = STM.atomically $ STM.writeTChan tc WinRefresh
+
 -- mouse related callbacks
 -- mouse press
-mouseButtonCallback tc worldState win GLFW.MouseButton'1 GLFW.MouseButtonState'Pressed _ = do 
-	putStrLn "."
-	STM.atomically $ do
-		world <- STM.readTVar worldState
-		(x,y) <- return $ lastPos world
-		-- test if we clicked on a system...........
-		case selectableSystems x y world of
-			[] -> STM.writeTVar worldState world {mouseDown=True, mouseDragging=False, selectedPoint=Nothing}
-			-- change mouse state and change selected system (if any)
-			s:xs ->	let	oldsel = selectedSys world
-					newsys = filter (s /=) $ oldsel:(systems world)
-	
-					pnt = selectPoint x y (baseShape s)
-				in STM.writeTVar worldState $ world {mouseDown=True, mouseDragging=False, systems=newsys,selectedSys=s,selectedPoint=Just pnt,firstPos=lastPos world}
+mouseButtonCallback tc worldState win GLFW.MouseButton'1 GLFW.MouseButtonState'Pressed _ = STM.atomically $ do
+	world <- STM.readTVar worldState
+	let	(x,y) = lastPos world
+		(newsys,sel,pnt) = pickSys x y world
+	STM.writeTVar worldState $ world	{mouseDown=True
+						,mouseDragging=False
+						,systems=newsys
+						,selectedSys=sel
+						,selectedPoint=pnt
+						,firstPos=lastPos world}
 
-	where	selectableSystems x y world = filter (canSelectSys x y)  ((selectedSys world):(systems world))
 
 -- mouse release
 mouseButtonCallback tc worldState win GLFW.MouseButton'1 GLFW.MouseButtonState'Released _ = STM.atomically $ do
 	world <- STM.readTVar worldState
-	STM.writeTVar worldState $ world {mouseDown=False, mouseDragging=False, selectedPoint=Nothing}
+	STM.writeTVar worldState $ world	{mouseDown=False
+						,mouseDragging=False
+						,selectedPoint=Nothing}
 -- catch-all mouseButton pattern
 mouseButtonCallback tc worldState win _ _ _ = return ()
 
 -- mouse move
 cursorPosCallback tc worldState win x y = STM.atomically $ do
 	world <- STM.readTVar worldState
+	-- test for mosue drag conditions
 	if mouseDown world && (mouseDragging world || distance (firstPos world) (x,y)>dragThresh) then
-		case selectedPoint world of
-			Just pnt ->	let newsys = snapTo x y (selectedSys world) pnt
-					in STM.writeTVar worldState $ world { mouseDragging=True, selectedSys=newsys, lastPos=(x,y) }
-			Nothing -> STM.writeTVar worldState world {lastPos=(x,y)}
+		let	sel = selectedSys world
+			pnt = selectedPoint world
+		 	sys = transformSys sel pnt
+		in STM.writeTVar worldState $ world	{mouseDragging=True
+							,selectedSys=sys
+							,lastPos=(x,y)}
 	else STM.writeTVar worldState world {lastPos=(x,y)}
+	where	transformSys sys Nothing = sys
+		transformSys sys (Just pnt) = snapTo x y sys pnt
 -- keyboard related callbacks
 keyCallback tc worldState win key sc keyState mod = putStrLn $ "key" ++ (show key) ++ (show sc) ++ (show keyState)
 	
 ---------- helper functions-------------	
--- resize function
-resize :: Int -> Int -> IO ()
-resize w h = do
-	GL.viewport $= (GL.Position 0 0, GL.Size (fromIntegral w) (fromIntegral h))
-	GL.matrixMode $= GL.Projection
-	GL.loadIdentity
-	GL.ortho 0 (realToFrac w) (realToFrac h) 0 (-1) 1
-	--GL.perspective 45 (fromIntegral w / fromIntegral h) 1 100
-	GL.matrixMode $= GL.Modelview 0
-
--- return the distance between 2 points
-distance :: (Floating a) => (a,a) -> (a,a) -> a
-distance (x1,y1) (x2,y2) = sqrt $ dx*dx + dy*dy 
-	where	dx = x2-x1
-		dy = y2-y1
+--
+-- pick a system to select out of the world. 
+pickSys :: Double -> Double -> World -> ([System],System,Maybe Int)
+pickSys x y world =	let	oldsel = selectedSys world
+				oldsys = systems world
+				(newsel,pnt) = pick selectable
+				newsys = filter (newsel /=) (oldsel:oldsys)
+			in (newsys,newsel,pnt)
+	where	selectable = filter (canSelectSys x y)  ((selectedSys world):(systems world)) -- returns a list of possible selectable systems
+		pick [] = (selectedSys world,Nothing)
+		pick (s:_) = (s,Just $ selectPoint x y (baseShape s))
 
 -- Snap system  point to x,y
 snapTo :: Double -> Double -> System -> Int -> System
@@ -108,11 +112,9 @@ snapShapeTo x y (Line (x1,y1) (x2,y2)) sel
 
 snapShapeTo x y (Circle (h,k) r) sel 
 	| sel==0 = Circle (x,y) r
-	| sel==1 = Circle (h,k) dist
+	| sel==1 = Circle (h,k) $ distance (x,y) (h,k)
 	| otherwise = Circle (h,k) r
-	where	dist = sqrt $ dx * dx + dy * dy
-		dx = x-h
-		dy = y-k
+snapShapeTo x y s _ = s -- unknown shape constructor
 
 -- returns true if System is within selectable distance of x,y
 canSelectSys :: Double -> Double -> System -> Bool
@@ -121,11 +123,8 @@ canSelectSys x y sys = canSelectShape x y $ baseShape sys
 canSelectShape :: Double -> Double -> Shape -> Bool
 canSelectShape x y (Point (px,py)) = x `closeTo` px && y `closeTo` py 
 canSelectShape x y (Line (x1,y1) (x2,y2)) = (x `closeTo` x1 && y `closeTo` y1) || (x `closeTo` x2 && y `closeTo` y2) 
-canSelectShape x y (Circle (h,k) r) = (x `closeTo` h && y `closeTo` k) || dist `closeTo` r
-	where	dist = sqrt $ dx * dx + dy * dy
-		dx = x-h
-		dy = y-k
-canSelectShape _ _ _ = False
+canSelectShape x y (Circle (h,k) r) = (x `closeTo` h && y `closeTo` k) || (distance (x,y) (h,k)) `closeTo` r
+canSelectShape x y _ = False --unknown shape constructor
 
 selectPoint :: Double -> Double -> Shape -> Int
 selectPoint x y (Point (px,py)) = 0
@@ -136,6 +135,7 @@ selectPoint x y (Line (x1,y1) (x2,y2))
 selectPoint x y (Circle (h,k) r) 
 	| (x `closeTo` h) && (y `closeTo` k) = 0
 	| otherwise = 0
+selectPoint x y _ = 0 --unknown shape constructor
 -- helper functions for selecting shapes
 closeToGen :: (Num a, Ord a) => a -> a -> a -> Bool
 closeToGen d a b = ( (b-d) < a && a < (b+d))
